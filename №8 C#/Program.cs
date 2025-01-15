0942a3085ad08bbe20d2fed0e38fb69d8a31f2e7
Matrosov <Matrosov@KOKSFENMETT>
2025-01-15 23:19:14 +0100

    Добавьте файлы проекта.

---
+++ №8 C#/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_C_
+{
+    class Cloak
+    {
+        public string Brand;
+        public string Gender;
+        public string Color;
+        public decimal Price;
+
+        public Cloak(string brand, string gender, string color, decimal price)
+        {
+            Brand = brand;
+            Gender = gender;
+            Color = color;
+            Price = price;
+        }
+
+        public void Info()
+        {
+            Console.WriteLine($"Brand: {Brand}, Gender: {Gender}, Color: {Color}, Price: {Price:F2}");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of coats: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Cloak[] cloaks = new Cloak[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"\nEnter data for cloak {i + 1}: ");
+                Console.Write("Brand: ");
+                string brand = Console.ReadLine();
+
+                Console.Write("Gender (male/female): ");
+                string gender = Console.ReadLine();
+
+                Console.Write("Color: ");
+                string color = Console.ReadLine();
+
+                Console.Write("Price: ");
+                decimal price = Convert.ToDecimal(Console.ReadLine());
+
+                cloaks[i] = new Cloak(brand, gender, color, price);
+            }
+
+            Console.Write("\nEnter gender (male/female): ");
+            string Gender = Console.ReadLine();
+
+            Console.Write("Enter the desired color: ");
+            string Color = Console.ReadLine();
+
+            Console.Write("Enter the maximum price: ");
+            decimal Price = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("\nRaincoats that meet your criteria:");
+
+            for (int i = 0; i < n; i++)
+            {
+                if(Gender == cloaks[i].Gender && Color == cloaks[i].Color && Price >= cloaks[i].Price)
+                {
+                    cloaks[i].Info();
+                }
+            }
+        }
+    }
+}
---
+++ №8 C#/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("№8 C#")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("№8 C#")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("08c4e63d-2844-4249-83be-3feefb141083")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
